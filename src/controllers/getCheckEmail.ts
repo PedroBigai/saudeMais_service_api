3defd26430b31caf261b5540440ea91520cd7c91
PedroBigai <jhoony.bigai@gmail.com>
2025-03-17 20:57:38 -0300

    upadting the verification email

--- src/controllers/getCheckEmail.ts
+++ src/controllers/getCheckEmail.ts
@@ -3,9 +3,17 @@ import { verifyEmail } from "../modules/verifyEmail";
 
 export const checkUserMail = async (req: Request, res: Response) => {
   try {
-    const { usuario } = req.params;
-    const disponivel = await verifyEmail(usuario);  // Chama o serviço
-    res.status(202).send({ disponivel });
+    const { email } = req.params;
+    const disponivel = await verifyEmail(email);  // Chama o serviço
+
+    if (disponivel) {
+      // Email disponível
+      res.status(200).send({ disponivel }); // Código 200 OK é mais apropriado para uma resposta bem-sucedida
+    } else {
+      // Email não disponível
+      res.status(409).send({ disponivel }); // Código 409 Conflict indica que o email já está em uso
+    }
+    
   } catch (error) {
     console.error("Erro ao verificar email:", error);
     res.status(500).send("Erro no servidor.");
--- src/server/routes.ts
+++ src/server/routes.ts
@@ -6,6 +6,7 @@ import { loadUserData } from "../modules/loadUserData";
 import verificarToken from "../modules/authService";
 import { updateDataController } from "../controllers/patchUserData";
 import { cadastrarUser } from "../controllers/postNewUser";
+import pool from "../utils/database";
 
 const router = Router();
 
@@ -17,7 +18,20 @@ router.post("/atualizar-peso", verificarToken, updateDataController);
 router.post("/cadastro", cadastrarUser);
 router.get("/teste", (req, res) => {
     res.json({ mensagem: "API funcionando corretamente! V1.0.0" });
-  });
+});
+router.get("/usuarios", async (req, res) => {
+    try {
+        // Fazendo o SELECT diretamente dentro da rota
+        const [rows, fields] = await pool.execute('SELECT * FROM usuarios');
+        
+        // Retorna os usuários como JSON
+        res.json(rows); 
+    } catch (error) {
+        console.error('Erro ao consultar usuários:', error);
+        res.status(500).json({ mensagem: 'Erro ao consultar usuários', erro: error });
+    }
+});
+
 
 export default router;
 
