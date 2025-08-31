b4f82c4208aafc62e9425ac11dd7149ac8c8af92
Lucas Hatz Cardoso <lucasxl13>
2025-08-31 09:06:16 -0300

    Remoção de listener duplo

--- src/server/app.ts
+++ src/server/app.ts
@@ -39,12 +39,4 @@ app.options("*", cors(corsOptions));
 // ---------- Rotas ----------
 app.use(router);
 
-// ---------- Server ----------
-const PORT = Number(process.env.PORT || 3000);
-const HOST = process.env.HOST || "0.0.0.0";
-
-app.listen(PORT, HOST, () => {
-  console.log(`Servidor rodando em http://${HOST}:${PORT}`);
-});
-
 export default app;
